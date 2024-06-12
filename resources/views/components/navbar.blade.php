948b844d9b1b8e3a31e88d99be2fbe1db6f64137
rajk <rajk80007@gmail.com>
2024-06-12 13:09:00 +0530

    Navbar in progress

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,15 @@
 <div>
-    <div class="flex items-center px-5 bg-black dark:bg-gray-900 w-[100%] h-[60px] text-white">
+    <div class="flex items-center px-5 bg-black dark:bg-gray-900 w-[100%] h-[60px] text-white justify-between">
+        <div>
+
+        </div>
+        <div>
+            <ul class="flex gap-4">
+                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Home</a></li>
+                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">About</a></li>
+                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Contact</a></li>
+            </ul>
+        </div>
         <ul class="flex gap-2">
             <li class="border-[1px] border-white py-1 px-3 rounded-lg hover:bg-slate-300 hover:text-gray-500 hover:font-bold hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"> <a href="#">Register</a></li>          
             <li class="border-[1px] border-white py-1 px-3 rounded-lg hover:bg-slate-300 hover:text-gray-500 hover:font-bold hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"> <a href="#">Login</a></li>
