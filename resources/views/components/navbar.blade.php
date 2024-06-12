78ac1a9016ddcd93d9ecc7b338c73237b7d6f89d
rajk <rajk80007@gmail.com>
2024-06-12 17:05:48 +0530

    Navbar almost completed

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="absolute left-3 top-3 cursor-pointer hover:border-white border-2 rounded-md block md:hidden">
+    <div onclick="menu.classList.toggle('hidden')" class="absolute left-3 top-3 cursor-pointer hover:border-white border-2 rounded-md block md:hidden">
         <img src="./assets/SVG/burgermenu.svg" alt="menu" width="30" height="30">
     </div>
     <div class="flex items-center px-5 bg-black dark:bg-gray-900 w-[100%] h-[60px] text-white justify-between">
@@ -7,14 +7,14 @@
         <div class="ml-12 md:ml-3">
             LogoBrand
         </div>
-        <div>
-            <ul class="flex gap-4 flex-col md:flex-row bg-gray-500 md:bg-black z-50">
-                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Home</a></li>
-                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">About</a></li>
-                <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Contact</a></li>
+        <div id="menu" class="duration-700 hidden md:block">
+            <ul class="md:ml-[70%] flex gap-3 items-center flex-col md:flex-row bg-gray-500 md:bg-black absolute left-0 top-[60px] opacity-40 w-[100%] pb-2 pt-2 hover:rounded-md text-center md:relative md:top-0 md:opacity-100">
+                <li class="hover:bg-red-700 hover:w-full"><a href="#" class="py-1 px-3 hover:font-bold duration-500 z-10 hover:w-[100%] hover:block ">Home</a></li>
+                <li class="hover:bg-red-700 hover:w-full"><a href="#" class="py-1 px-3 hover:font-bold duration-500 z-10 hover:w-[100%] hover:block ">About</a></li>
+                <li class="hover:bg-red-700 hover:w-full"><a href="#" class="py-1 px-3 hover:font-bold duration-500 z-10 hover:w-[100%] hover:block ">Contact</a></li>
             </ul>
         </div>
-        <ul class="flex gap-2">
+        <ul class="flex gap-2 scale-75 md:scale-100 ml-[25%]">
             <li class="border-[1px] border-white py-1 px-3 rounded-lg hover:bg-slate-300 hover:text-gray-500 hover:font-bold hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"> <a href="#">Register</a></li>          
             <li class="border-[1px] border-white py-1 px-3 rounded-lg hover:bg-slate-300 hover:text-gray-500 hover:font-bold hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"> <a href="#">Login</a></li>
         </ul>
