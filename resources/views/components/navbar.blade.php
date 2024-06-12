b902ee903060b0e21b051e625a8152cb3b61ba7e
rajk <rajk80007@gmail.com>
2024-06-12 14:58:42 +0530

    Navbar Hamberger

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,14 @@
 <div>
+    <div class="absolute left-3 top-3 cursor-pointer hover:border-white border-2 rounded-md block md:hidden">
+        <img src="./assets/SVG/burgermenu.svg" alt="menu" width="30" height="30">
+    </div>
     <div class="flex items-center px-5 bg-black dark:bg-gray-900 w-[100%] h-[60px] text-white justify-between">
-        <div>
-
+        
+        <div class="ml-12 md:ml-3">
+            LogoBrand
         </div>
         <div>
-            <ul class="flex gap-4">
+            <ul class="flex gap-4 flex-col md:flex-row bg-gray-500 md:bg-black z-50">
                 <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Home</a></li>
                 <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">About</a></li>
                 <li><a href="#" class="hover:bg-red-700 hover:font-bold duration-300 p-2">Contact</a></li>
